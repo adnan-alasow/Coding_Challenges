0de0cb24e53cfc2d294ef5f1e12922d7f5162c61
Adnan Alasow <mearone.omar@gmail.com>
2023-07-01 16:59:00 -0700

    completing some HTML edits

--- script.js
+++ script.js
@@ -29,9 +29,62 @@ Variables
 1. Write a program that declares two variables, one with your name and 
 one with a number value of your age. Concatenate the two variables and 
 output the result to the console.
+
+2. Repeat the coding task 2 of output by putting these strings in variables 
+and then outputting variables.
 */
 
 let myName = 'Adnan';
 let age = 30;
+let myNameAndAge = myName + age;
+
+console.log(myNameAndAge);
 
 console.log("My name is " + myName + ", and I am " + age + " years old.");
+
+/*
+3. Initialize two variables and output their sum, minus, multiply and 
+division.
+*/
+
+let numberOne = 10;
+let numberTwo = 20;
+
+console.log(numberOne + numberTwo);
+console.log(numberTwo - numberOne);
+console.log(numberOne * numberTwo);
+console.log(numberTwo / numberOne);
+
+/*
+BMI 
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using 
+the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg and height in meter).
+Note - The syntax for exponentiation is two asterisks in a row ( ** )
+Your Tasks
+1. Store Mark's and John's mass and height in variables 
+2. Calculate both their BMIs using the formula 
+3. Create a Boolean variable 'markHigherBMI' containing information about whether Mark 
+has a higher BMI than John.
+*/
+
+let marksMass = 77; // 170 pound in kg 77.1107
+let marksHeight = 1.8034; // 5'11" in meters 1.8034
+let marksBmiMass = marksMass / (marksHeight ** 2);
+let marksBmi = marksBmiMass / marksHeight * marksHeight;
+
+let johnsMass = 70; // 154 pound in kg 69.8532
+let johnsHeight = 1.91; //6'3" in meters 1.905 
+let johnsBmiMass = johnsMass / (johnsHeight ** 2);
+let johnsBmi = johnsBmiMass / johnsHeight * johnsHeight;
+
+let markHigherBMI = marksBmi > johnsBmi;
+console.log(marksBmi);
+console.log(johnsBmi);
+console.log(markHigherBMI);
+
+
+
+
+
+
